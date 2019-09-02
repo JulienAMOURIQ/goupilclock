b706d35f5889ce7d6a2e42edc1153d7e44f2e683
Julien AMOURIQ <jamouriq@fedora-i3.home>
2019-09-02 21:03:05 +0200

    création du daemon

--- src/goupilclock/application.py
+++ src/goupilclock/application.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import daemon
+
+
+def main():
+    print("tst")
+    #https://www.python.org/dev/peps/pep-3143/
+
+with daemon.DaemonContext():
+    main()
