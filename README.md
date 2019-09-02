771fad7074ad72d7b8c37df43b9dbbd579a36831
Julien AMOURIQ <jamouriq@fedora-i3.home>
2019-09-02 20:35:17 +0200

    structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Goupilclock
+
+Bienvenue sur le site de développement de Goupilclock, le système de limitation de l'usage du PC sous linux.
---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='goupilclock',
+      version='1.0',
+      description='Logiciel de limitation horaire',
+      url='https://github.com/JulienAMOURIQ/goupilclock',
+      author='Julien AMOURIQ',
+      author_email='julien.amouriq@laposte.net',
+      license='GPL3',
+      packages=['goupilclock'],
+      zip_safe=False)
